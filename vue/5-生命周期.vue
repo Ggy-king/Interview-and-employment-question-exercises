a694ccf9e06e91e7072a4e3870f6ed3c7e02f4f8
king <102906132+Ggy-king@users.noreply.github.com>
2022-09-22 21:50:12 +0800

    Add files via upload

---
+++ vue/5-生命周期.vue
@@ -0,0 +1,40 @@
+<template>
+<!-- 挂载   -->
+<!-- 更新   -->
+<!-- 销毁   -->
+
+
+/**
+{
+    new vue()
+    初始化 - 事件/生命周期
+    --beforeCreate
+    初始化 - 注入/校验
+    --created
+    (是否指定 el 选项) - 否 - 当vm.$mount(el)函数时 - 是 - 是否指定template选项
+    (是) -> 将template编译到render函数中
+    (否) -> 将el外部的HTML作为template编译
+    --beforeMount
+    创建vm.$el并用其替换el
+    --mounted
+    挂载完毕 - (当data被修改时) --beforeUpdate -> 虚拟dom重新渲染并应用更新 --updated
+    当vm.$destroy()函数时
+    --beforeDestroy
+    解除绑定销毁子组件以及事件监听器
+    销毁完毕
+    --destroyed
+
+}
+
+*/
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/6-vue高级特性.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- vue高级特性 -->
+  <p>1 不是每一个都常用 但用到的时候要知道</p>
+  <p>2 考察候选人对vue的掌握是否全面 且有深度</p>
+  <p>3 考察做过的项目是否有深度和复杂度</p>
+  <main>
+    <p>1 自定义v-model</p>
+    <p>2 $nextTick</p>
+    <p>3 slot</p>
+    <p>4 动态、异步组件</p>
+    <p>5 keep-alive</p>
+    <p>6 mixin</p>
+    <p>7 refs</p>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
