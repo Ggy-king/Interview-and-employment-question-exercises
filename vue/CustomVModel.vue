5afe712fa40af79a0a717d0d359ca0ce93c53457
king <102906132+Ggy-king@users.noreply.github.com>
2022-09-23 19:50:52 +0800

    Add files via upload

---
+++ vue/CustomVModel.vue
@@ -0,0 +1,32 @@
+<template>
+    <!--比如 vue颜色选择 -->
+    <input type="text" 
+        :value="text"
+        @input="$emit('change',$event.target.value)"
+    >
+
+    <!-- 
+    1 上面的 input 使用了 :value 而不是 v-model
+    2 上面的change和model.event对应起来
+    3 text属性要对应起来 
+    -->
+</template>
+
+<script>
+export default {
+    model: {
+        prop: 'text',  //对应props text
+        event: 'change'
+    },
+    props: {
+        text: String,
+        default() {
+            return ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
