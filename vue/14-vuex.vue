2a04f20efa07c36446e1189dc3f92379ff4c9f6a
king <102906132+Ggy-king@users.noreply.github.com>
2022-10-01 21:56:57 +0800

    Add files via upload

---
+++ vue/14-vuex.vue
@@ -0,0 +1,34 @@
+<template>
+ <main>
+    <!-- vuex的基本概念 -->
+    <p>state</p>
+    <p>getters</p>
+    <p>action</p>
+    <p>mutation</p>
+
+    <!-- vuex流程图 -->
+    {
+        vue Components -- Dispatch --> Actions -- Commit --> Mutations --Mutate -->State -- render --> vue Components
+
+        actions中可以做一些异步的操作 Backend API
+    }
+
+    <!-- 拥有vue组件 -->
+    <p>dispatch</p>
+    <p>commit</p>
+    <p>mapState</p>
+    <p>mapGetters</p>
+    <p>mapActions</p>
+    <p>mapMutations</p>
+ </main>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/15-vue-router.vue
@@ -0,0 +1,52 @@
+<template>
+
+<!-- 1 路由模式(hash H5 history) -->
+<!-- 2 路由配置(动态路由 懒加载) -->
+ 
+
+ 
+</template>
+
+<script>
+// export default {
+
+//     setup(props) {
+//         const User = {
+//             //获取参数10 20
+//             template: '<div>User {{$route.params.id}}</div>'
+//         }
+//         const router = new VueRouter({
+//             routes: [
+//                 //动态路径参数 以冒号开头 可以命中'/user/10' '/user/20' 等路径的格式
+//                 {
+//                     path: 'user/:id',components:User
+//                 } 
+//             ]
+//         })
+//     }
+// }
+
+// 路径组件的懒加载
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: () => import(
+                /* webpackChunkName:"home" */
+                '组件路径'
+            )
+        },
+        {
+            path: '/acc',
+            component: () => import(
+                /* webpackChunkName:"acc" */
+                '组件路径'
+            )
+        }
+    ]
+})
+</script>
+
+<style>
+
+</style>
