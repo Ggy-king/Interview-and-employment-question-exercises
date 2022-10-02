b883d9a2d18d54adeae515ba1c528355530942ad
king <102906132+Ggy-king@users.noreply.github.com>
2022-10-02 20:45:35 +0800

    Add files via upload

---
+++ vue/16-vue原理.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <p>1 组件化</p>
+    <p>2 响应式</p>
+    <p>3 vdom和diff</p>
+    <p>4 模板编译</p>
+    <p>5 渲染过程</p>
+    <p>6 前端路由</p>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
