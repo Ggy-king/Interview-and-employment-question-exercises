04c42a4a89f861a5d5f58307c300d470ce7ebd3a
king <102906132+Ggy-king@users.noreply.github.com>
2022-10-03 16:14:07 +0800

    Add files via upload

---
+++ vue/17-组件化基础MVVM.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- 组件化基础 -->
+    <!-- MVVM模型 -->
+    <main>
+        <ul>
+            <li>1 很久之前就出现了组件化</li>
+            <!-- asp jsp php 有组件化 -->
+            <!-- nodejs 也有组件化 -->
+
+
+            <li>2 数据驱动视图(MVVM,setState)</li>
+            <!-- 传统组件 只是静态渲染 更新还是需要依赖于操作Dom 当年jQuery流行的原因 -->
+            <!-- 数据驱动视图 - Vue MVVM -->
+            <!-- 数据驱动视图 - React setState -->
+
+            view(DOM) -- ViewModel(去DOM Listeners 回Directives) -- model(Plain JS Object)
+        </ul>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/18-vue响应式.vue
@@ -0,0 +1,31 @@
+<!-- Vue响应式 -->
+<template>
+    <!-- 1 组件data的数据变化 立即触发视图更新 -->
+
+    <!-- 2 实现数据驱动视图的第一步 -->
+
+    <!-- 3 考餐Vue原理(一) -->
+
+    <p>
+        核心API - Object.defineProperty(get set的使用)
+        监听对象 监听数组
+        复杂对象 深度监听
+        缺点
+    </p>
+    <p>
+        Object.defineProperty 的一些缺点 vue3.0启用Proxy
+    </p>
+    <p>
+        Proxy兼容性不好 且无法使用polyfill
+    </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
