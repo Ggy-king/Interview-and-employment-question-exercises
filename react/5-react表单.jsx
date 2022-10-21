0bf00902da2421e511e480dfa1f06163920758d9
king <102906132+Ggy-king@users.noreply.github.com>
2022-10-21 21:12:59 +0800

    Add files via upload

---
+++ react/5-react表单.jsx
@@ -0,0 +1,11 @@
+// 表单
+
+// 1 受控组件
+// vue中v-model
+// onChange使用 实际上react是自己实现onChange方法
+
+
+// 2 input textarea select 都是使用value方法
+
+
+// 3 checkbox radio 用checked
---
+++ react/6-react父子间组件通讯.jsx
@@ -0,0 +1,12 @@
+//1 props传递数据
+// vue需要emit 再props接
+// react 在组件上直接写 vue也可以这样
+
+
+// 2 props传递函数
+
+// 3 props类型检查
+// 绑定在原型上
+// list.prototype = {
+//     list:PropTypes.arrayOf(PropTypes.object).isRequired
+// }
