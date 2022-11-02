f30468f0f77fa08e56f49aba83b214cd7996f6e2
king <102906132+Ggy-king@users.noreply.github.com>
2022-11-02 21:47:03 +0800

    Add files via upload

---
+++ react/29-jsx的本质.jsx
@@ -0,0 +1,21 @@
+// 1 jsx等同于vue模本
+// 2 vue模板不是html
+// 3 jsx也不是js
+
+// babel官网
+
+
+// jsx编译后 
+React.createElement('div', null, child1, child2, child3)
+React.createElement(List, null, child1, child2, child3)
+// h函数
+// vnode返回
+// patch
+
+
+
+
+// jsx本质
+// React.createElement()  即  h函数，返回vnode
+// 第一个参数可能是组件 也可能是html tag
+// 组件名首字母大写
