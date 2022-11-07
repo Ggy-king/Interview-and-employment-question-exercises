09d41fdac72d8c85bcc7d012214be6c57c647c42
king <102906132+Ggy-king@users.noreply.github.com>
2022-11-07 17:28:25 +0800

    Add files via upload

---
+++ webpack/2-webpack基本配置.js
@@ -0,0 +1,11 @@
+// cli工程师 指的是只使用过vue-cli create-react-app 自己不会配置webpack
+
+const webpack = require("webpack")
+
+// 定义 webpack.common.js webpack.prod.js webpack.dev.js 三个文件
+
+/**
+ * 1 拆分配置和merge
+ * 2 启动服务
+ */
+
