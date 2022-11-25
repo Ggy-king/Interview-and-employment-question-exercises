07df3ff8d4191ca951b40a893222f1e64edf4b3b
king <102906132+Ggy-king@users.noreply.github.com>
2022-11-25 21:12:18 +0800

    Add files via upload

---
+++ nodejs/nodejs基本概念.js
@@ -0,0 +1,34 @@
+// 1 nodejs是什么
+/**
+ * nodejs是基于chome v8引擎的javascript运行时
+ * nodejs出现之前 js只能在浏览器上运行
+ * nodejs出现之后 js可以在任何安装nodejs的环境下运行
+ */
+
+
+// 2 commonjs和es6 module有何区别
+/**
+ * 语法不同
+ * commonjs是动态引入 执行时引入
+ * es6是静态引入 编译时引入
+ */
+
+// commonjs require三个级别
+/**
+ * nodejs 自带模块 
+ * npm安装的模块
+ * 自定义的模块
+ */
+
+
+// 3 nodejs和前端js区别
+/** 
+ * 都使用es语法 如变量 函数 class
+ * 前端js使用JS Web Api  如DOM BOM ajax
+ * nodejs使用 node Api 如http querystring
+ * 
+ * 
+ * 前端js应用于网页 在浏览器上运行
+ * nodejs可用于服务端 如开发web serve
+ * nodejs也可以用于本机 如webpack等本机工具
+*/
---
+++ nodejs/处理http请求.js
@@ -0,0 +1,17 @@
+// 1 如何启动http服务
+
+// 2 如何定义路由
+/**
+ * 两大要素 method+pathname
+ * 从req中获取method和url
+ * 再通过url获取pathname
+ */
+// 3 如何获取querystring
+// 4 如何获取request body
+// 5 如何返回json格式
+// 6 session如何实现登录
+/**
+ * cookie可实现登录校验
+ * cookie不能泄露用户信息 所以有了session
+ * session存储用户信息 cookie和session关联
+ */
