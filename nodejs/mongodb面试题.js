7bfea9fe0160385309b96060c515c184cb9cb416
king <102906132+Ggy-king@users.noreply.github.com>
2022-11-26 13:04:42 +0800

    Add files via upload

---
+++ nodejs/mongodb面试题.js
@@ -0,0 +1,19 @@
+// 1 DB Collect Document 分别是什么意思
+/**
+ * DB - 数据库 一个系统的数据集合 如留言板项目
+ * Collect - 集合 一类数据的集合 如所有的用户数据
+ * Document - 文档 单个数据 如一个用户的数据
+ */
+// 2 Mongoose的Schema 和 Model是什么意思
+/**
+ * Schema - 数据规范，定义哪些属性 数据类型 是否必需
+ * Model - 数据模型 提供API操作Document
+ * Model依赖一个Schema规范 对应一个collection
+ */
+
+// 3 是否用过数据库Myspl
+/**
+ * 1 暂时没用过 正在准备学习
+ * 2 Myspl是关系型数据库 需要设计数据表
+ * 2 Mysql需要SQL语句操作
+ */
